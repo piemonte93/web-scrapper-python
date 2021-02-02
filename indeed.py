b695448aeddf5492625e1c3d04893c684d07c344
ChaDongHyun <b07882002@gmail.com>
2021-02-03 03:35:05 +0900

    함수명 변경 및 내용 변경
    extract_jobs(last_page)에서 반복문 수행마다 그냥 페이지만 출력하던 것에서 indeed의 몇 페이지를 출력하도록 변경

--- indeed.py
+++ indeed.py
@@ -5,7 +5,7 @@ LIMIT = 50
 URL = f"https://kr.indeed.com/%EC%B7%A8%EC%97%85?as_and=python&as_phr=&as_any=&as_not=&as_ttl=&as_cmp=&jt=all&st=&salary=&radius=25&l=&fromage=last&limit={LIMIT}"
 
 
-def extract_indeed_pages():
+def get_last_page():
     # requests Obj 안의 get()메소드 이용
     result = requests.get(URL)
 
@@ -42,14 +42,21 @@ def extract_job(html):
     return {"title": title, "company": company, "location": location, "link": f"https://kr.indeed.com/%EC%B1%84%EC%9A%A9%EB%B3%B4%EA%B8%B0?jk={job_id}"}
 
 
-def extract_indeed_jobs(last_page):
+def extract_jobs(last_page):
     jobs = []
     for page in range(last_page):
-        print(f"Scrapping page {page}")
+        print(f"Scrapping Indeed: page {page}")
         result = requests.get(f"{URL}&start={page * LIMIT}")
         soup = BeautifulSoup(result.text, 'html.parser')
         results = soup.find_all("div", {"class": "jobsearch-SerpJobCard"})
         for result in results:
             job = extract_job(result)
             jobs.append(job)
+    return jobs
+
+
+def get_jobs():
+    last_page = get_last_page()
+    jobs = extract_jobs(last_page)
+
     return jobs
